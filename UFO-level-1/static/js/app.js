4c18fb7d11cea71d089cd38625f8fe7ffa58d968
jinzi860209 <jinzi860209@gmail.com>
2020-12-21 22:12:30 -0500

    Update

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,19 +2,20 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+// Get a reference to the table body
 var tbody = d3.select("tbody");
-
+// Console.log the UFO sightings data from data.js
 console.log(data);
-
+// Loop Through `data` and console.log each UFO sightings object
 data.forEach(function(ufoSightings) {
     console.log(ufoSightings);
 });
-
+// Use d3 to append one table row `tr` for each UFO sightings object
 data.forEach(function(ufoSightings) {
     console.log(ufoSightings);
     var row = tbody.append("tr");
 });
-
+// Use `Object.entries` to console.log each UFO sightings value
 data.forEach(function(ufoSightings) {
     console.log(ufoSightings);
     var row = tbody.append("tr");
@@ -23,7 +24,7 @@ data.forEach(function(ufoSightings) {
         console.log(key, value);
     });
 });
-
+// Use d3 to append 1 cell per UFP sightings value (datetime, city, state, country, shape, durationMinutes, comments)
 data.forEach(function(ufoSightings) {
     console.log(ufoSightings);
     var row = tbody.append("tr");
@@ -33,7 +34,7 @@ data.forEach(function(ufoSightings) {
         var cell = tbody.append("td");
     });
 });
-
+// Use d3 to update each cell's text with (datetime, city, state, country, shape, durationMinutes, comments)
 data.forEach(function(ufoSightings) {
     console.log(ufoSightings);
     var row = tbody.append("tr");
@@ -43,7 +44,7 @@ data.forEach(function(ufoSightings) {
         cell.text(value);
     });
 });
-
+// Refactor to use Arrow Functions
 data.forEach((ufoSightings) => {
     var row = tbody.append("tr");
     Object.entries(ufoSightings).forEach(([key, value]) => {
